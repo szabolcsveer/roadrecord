d8601d2fcff31e50381faf77f5ce7fcc3b6db3cc
szabolcsveer <szabolcs.veer@gmail.com>
2020-01-15 15:05:35 +0100

    feat(settings.py) connected to db

--- roadrecord/settings.py
+++ roadrecord/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'roadrecord.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'roadrecord2',
+        'USER': 'szabi',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
